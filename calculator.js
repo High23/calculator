d30188fad0bae253ec0260251271d0846ae7ae64
High23 <warenasb2@gmail.com>
2023-05-27 00:26:42 -0700

    Works with single digit numbers

--- calculator.js
+++ calculator.js
@@ -1,7 +1,3 @@
-let userInputNumber = 0;
-let userInputOperator = '+';
-let userInputNumberTwo = 0;
-
 function add(numberOne, numberTwo){
     return numberOne + numberTwo;
 }
@@ -29,11 +25,59 @@ function operate(number, operator, numberTwo){
         return divide(number, numberTwo);
     }
 }
+let userInputNumber = '';
+let userInputOperator = '';
+let userInputNumberTwo = '';
+let counter = 0;
+
 const display = document.querySelector("div.display.flex");
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        display.textContent = button.value;
+        if (button.value === "clear" || (counter == 0 && button.value == '=')){
+            userInputNumber = '';
+            userInputOperator = '';
+            userInputNumberTwo = '';
+            counter = 0;
+            display.textContent = '0';
+            return;
+        }
+        if (button.value == '+' || button.value == '-' || button.value == '/' || button.value == '*' || button.value == '%'){
+                userInputOperator = button.value;
+                console.log(userInputOperator);
+                counter = 1;
+                return;
+        }
+        if (counter === 1 && !(userInputNumber == '')){
+            userInputNumberTwo += button.value;
+            if (button.value == '='){
+                if (userInputNumberTwo == '='){
+                    userInputNumberTwo = '';
+                    return;
+                }
+                userInputNumber = operate(Number(userInputNumber), userInputOperator, Number(userInputNumberTwo));
+                display.textContent = userInputNumber;
+                userInputNumberTwo = '';
+                console.log(button.value);
+                return;
+            } else if (userInputOperator && !(userInputNumberTwo == '')){
+                userInputNumber = operate(Number(userInputNumber), userInputOperator, Number(userInputNumberTwo));
+                display.textContent = userInputNumber;
+                userInputNumberTwo = '';
+                console.log(userInputOperator);
+                return;
+            }
+            display.textContent = userInputNumberTwo;
+            console.log(userInputNumberTwo);
+        } else {
+            if (button.value == '='){
+                counter = 0;
+                return;
+            }
+            userInputNumber += button.value;
+            display.textContent = userInputNumber;
+            console.log(userInputNumber);
+        } 
     });
 });
 console.log(operate(userInputNumber, userInputOperator, userInputNumberTwo));
