bc102cf9b92a9eda22da6ee9419b769b47487275
High23 <warenasb2@gmail.com>
2023-05-27 20:40:25 -0700

    Added backspace and modular division functionality

--- calculator.js
+++ calculator.js
@@ -13,6 +13,9 @@ function multiply(number, numberTwo){
 function divide(number, numberTwo){
     return number / numberTwo;
 }
+function modularDivision(number, numberTwo){
+    return number % numberTwo;
+}
 
 function operate(number, operator, numberTwo){
     if (operator === '+'){
@@ -23,6 +26,8 @@ function operate(number, operator, numberTwo){
         return multiply(number, numberTwo);
     } else if (operator === '/'){
         return divide(number, numberTwo);
+    } else {
+        return modularDivision(number, numberTwo);
     }
 }
 let userInputNumber = '';
@@ -32,15 +37,15 @@ let counter = 0;
 
 const display = document.querySelector("div.display.flex");
 const numberButtons = document.querySelectorAll("div.numbers > button");
-const operatorButtons = document.querySelectorAll("div.ops > button");
-const extraOpsButtons = document.querySelectorAll("div.extra-ops > button");
+const operatorButtons = document.querySelectorAll("button.opsSelector");
+const extraOpsButtons = document.querySelectorAll("button.exOps");
 const decimal = document.querySelector("#decimal")
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {   
         if (button.value == '.') decimal.disabled = true;
         if (counter === 1 && !(userInputNumber == '')){
             if (button.value == '='){
-                if (userInputOperator == '/' && userInputNumberTwo == '0') return display.textContent = LOL;
+                if (userInputOperator == '/' && userInputNumberTwo == '0') return display.textContent = 'LOL';
                 userInputNumber = operate(Number(userInputNumber), userInputOperator, Number(userInputNumberTwo));
                 display.textContent = userInputNumber;
                 userInputNumberTwo = '';
@@ -50,10 +55,7 @@ numberButtons.forEach((button) => {
             userInputNumberTwo += button.value;
             display.textContent = userInputNumberTwo;
         } else {
-            if (button.value == '='){
-                counter = 0;
-                return;
-            }
+            if (button.value == '=') return counter = 0;
             userInputNumber += button.value;
             display.textContent = userInputNumber;
         } 
@@ -62,7 +64,7 @@ numberButtons.forEach((button) => {
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () =>{
         if (userInputOperator && !(userInputNumberTwo == '')){
-            if (userInputOperator == '/' && userInputNumberTwo == '0') return display.textContent = LOL;
+            if (userInputOperator == '/' && userInputNumberTwo == '0') return display.textContent = 'LOL';
             userInputNumber = operate(Number(userInputNumber), userInputOperator, Number(userInputNumberTwo));
             display.textContent = userInputNumber;
             userInputNumberTwo = '';
@@ -73,15 +75,38 @@ operatorButtons.forEach((button) => {
     });
 });
 extraOpsButtons.forEach((button) => {
-    button.addEventListener('click', () =>{
+    button.addEventListener('click', () =>{ 
         if (button.value === "clear"){
             userInputNumber = '';
             userInputOperator = '';
             userInputNumberTwo = '';
             counter = 0;
-            display.textContent = '0';
+            display.textContent = 0;
             return;
+        } else if (button.value === "back" && !(userInputNumberTwo === '') && counter === 1){
+            userInputNumberTwo = userInputNumberTwo.split("");
+            if (userInputNumberTwo.length <= 1){
+                counter = 1;
+                userInputNumberTwo = "";
+                return display.textContent = 0;
+            } else if (Array.isArray(userInputNumberTwo)){
+                counter = 1;
+                userInputNumberTwo.pop()
+                userInputNumberTwo = userInputNumberTwo.join("");
+                return display.textContent = userInputNumberTwo;
+            }
+        }
+        userInputNumber = userInputNumber.split("");
+        if (userInputNumber.length <= 1){
+            userInputNumber = "";
+            counter = 0;
+            return display.textContent = 0;
+        } else if (Array.isArray(userInputNumber)){
+            counter = 0;
+            userInputNumber.pop()
+            userInputNumber = userInputNumber.join("");
+            return display.textContent = userInputNumber;
         }
     });
-    
+
 });
